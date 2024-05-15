48170504e0ea5dbf01d1bd5d85acc392106257b4
rohit <yadav@Aspire.com>
2024-05-15 23:15:15 +0530

    first commit

---
+++ src/test/java/com/SpringSecurity/SprringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
